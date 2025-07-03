6515ef47f8a3ce36b95e7ff8e2d686e1bc7b4f3c
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2025-07-03 17:45:47 -0400

    removing lib (prepping for submodule)

--- src/main/java/org/team2342/lib/leds/LedIOCANdle.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2025 Team 2342
-// https://github.com/FRCTeamPhoenix
-//
-// This source code is licensed under the MIT License.
-// See the LICENSE file in the root directory of this project.
-
-package org.team2342.lib.leds;
-
-public class LedIOCANdle implements LedIO {}
